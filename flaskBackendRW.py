961f6745bfd4775d3abf3d6c410933ef2449cf16
rsleezer <74322951+rsleezer@users.noreply.github.com>
2024-10-25 13:18:54 -0500

    Add files via upload (#2)
    * Changed main, Signup, and a bit of backend
    
    * Changes to active btn For header
    
    * Flash Message and requirement changes
    
    * Added a profile image once someone signs in
    
    * Changes to Password entry and some comments
    
    * Comments
    
    * Added profile and settings html files
    
    ---------
    
    Co-authored-by: Aidan-Rosenboom <awrosenboom@gmail.com>
    Co-authored-by: Aidan-Rosenboom <112183322+Aidan-Rosenboom@users.noreply.github.com>

--- flaskBackendRW.py
+++ flaskBackendRW.py
@@ -1,74 +1,105 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash, redirect, session
+from flask_bcrypt import Bcrypt
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "Aidan"
+bcrypt = Bcrypt(app)
 
 mydb = mysql.connector.connect(
-    host="srv1356.hstgr.io",
-    user="u612285796_root",
-    password="ILoveMae123",
-    database="u612285796_firstcohort")
+  host="srv1356.hstgr.io",
+  user="u612285796_root",
+  password="ILoveMae123",
+  database="u612285796_firstcohort",
+  pool_size= 3) 
 
 @app.route('/')
 def home():
-    return render_template('HomeRW.html')
+  return render_template('HomeRW.html',page='Home')
 
 @app.route('/Our_Program')
 def ourProgram():
-    return render_template('OurProgram.html')
+  return render_template('OurProgram.html',page='About')
 
 @app.route('/Minnpoly')
 def minnpoly():
-    return render_template('Minnpoly.html')
+  return render_template('Minnpoly.html',page='About')
 
 @app.route('/Industry_Partners')
 def indPart():
-    return render_template('Indpart.html')
+  return render_template('Indpart.html',page='About')
 
 @app.route('/Prerequisites/MSU')
 def prereqMSU():
-    return render_template('prereqMSU.html')
+  return render_template('prereqMSU.html',page='Preq')
 
 @app.route('/Prerequisites/Courses')
 def prereqCourses():
-    return render_template('prereq.html')
+  return render_template('prereq.html',page='Preq')
 
 @app.route('/Apply')
 def apply():
-    return render_template('Application.html')
+  return render_template('Application.html',page='Apply')
 
 @app.route('/Frequently_Asked_Questions')
 def faq():
-    return render_template('FAQRW.html')
+  return render_template('FAQRW.html',page='FAQ')
 
 @app.route('/Calendar')
 def calendar():
-    return render_template('Calendar.html')
+  return render_template('Calendar.html')
+
+@app.route('/Profile')
+def profile():
+    return render_template('profile.html', page='Profile')
+
+@app.route('/Settings')
+def accntSettings():
+  return render_template("Settings.html")
 
 @app.route('/Login')
 def login():
-    return render_template('LoginRW.html')
+  return render_template('LoginRW.html')
 
-@app.route('/SignUp')
-def signUp():
-    return render_template('SignUpRW.html')
+@app.route('/SignUp', methods=['GET','POST'])
+def submit():
+  cursor = mydb.cursor()
+  if request.method == 'POST':
+    myform = request.form
+    #gets the data from the signUp form
+    firstName = myform['first']
+    lastName = myform['last']
+    email = myform['email']
+    password = myform['password']
+    #generates hashcode for the password entered
+    hashPass = bcrypt.generate_password_hash(password).decode("utf-8")
+    starId = myform['starid']
+    phone = myform['phone']
+    #cursor finds duplicate entries (if there are any)
+    cursor.execute("SELECT email FROM profiles WHERE email = %s", (email,))
+    dup = cursor.fetchone()
+    #checks for duplicate email entries
+    if dup:
+      cursor.close()
+      flash("You already have an account Please login.")
+      return redirect(url_for('login'))
+    else:
+      #inserts the new entry into the profiles table in MySql
+      cursor.execute("INSERT INTO profiles (first_name, last_name, phone, email, starid, password) VALUES (%s,%s,%s,%s,%s,%s)", ( firstName, lastName, phone, email, starId, hashPass))
+      mydb.commit()
+      #logs the user in, session uses email to identify who is signed in
+      session['user'] = email
+      cursor.close()
+      #I want to look into changing the color of the flash box for this statement
+      flash("Success: Your all signed up!")
+      return redirect(url_for('home'))
+  cursor.close()
+  return render_template("SignUpRW.html")
 
-#student submit also add a faculty submit function
-@app.route('/submit', methods=['POST'])
-def submitStudent():
-    if request.method == 'POST':
-        myform = request.form
-        firstName = myform['first']
-        lastName = myform['last']
-        email = myform['email']
-        password = myform['password']
-        starId = myform['starid']
-        phone = myform['phone']
-        cursor = mydb.cursor()
-        cursor.execute("INSERT INTO student (first_name, last_name, phone, email, starid, password) VALUES (%s,%s,%s,%s,%s,%s)", (firstName, lastName, phone, email, starId, password))
-        mydb.commit()
-        cursor.close()
-        return "Success"
-
+@app.route('/Logout')
+def logout():
+  session.pop('user', None)
+  return redirect(url_for('home'))
+  
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
